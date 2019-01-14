96814c5ed96e36f07e6729591e27cc0c6ab194eb
Fadil Abdella <fabdella03@gmail.com>
2019-01-14 16:51:31 -0500

    correction1

--- assets/javascript/pro.js
+++ assets/javascript/pro.js
@@ -1,7 +1,5 @@
 
-
-
-var clientID = "103971723552-j63qs9m5um73bdmpfkskbg0d5v7mgvm2.apps.googleusercontent.com";
+var clientId = "103971723552-j63qs9m5um73bdmpfkskbg0d5v7mgvm2.apps.googleusercontent.com";
 
 // Array of API discovery doc URLs for APIs used by the quickstart
 var DISCOVERY_DOCS = ["https://www.googleapis.com/discovery/v1/apis/youtube/v3/rest"];
